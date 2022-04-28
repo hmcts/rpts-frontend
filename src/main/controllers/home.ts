c3e71f7040161db60c3dfe9135d3f4a4b48755ca
amstevenson <addstevenson@hotmail.com>
2022-04-28 15:16:06 +0100

    starting page for postcode lookup

--- src/main/controllers/home.ts
+++ src/main/controllers/home.ts
@@ -2,6 +2,22 @@ import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', (req, res) => {
-    res.render('home');
+
+    res.render('home', {
+      'errorMsg': '',
+      'emptyValueFound': false,
+      'postcodeInvalidFormat': false,
+      'postcodeEntered': ''
+    });
+  });
+
+  app.get('/:postcode/results', (req, res) => {
+
+    res.render('home', {
+      'errorMsg': '',
+      'emptyValueFound': false,
+      'postcodeInvalidFormat': false,
+      'postcodeEntered': req.params.postcode
+    });
   });
 }
