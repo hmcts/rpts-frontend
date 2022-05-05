705d4d318be4ef76da70149b2af093eb02478e3e
amstevenson <addstevenson@hotmail.com>
2022-05-05 11:57:30 +0100

    result address showing on page

---
+++ src/main/api/rptsApi.ts
@@ -0,0 +1,32 @@
+const { Logger } = require('@hmcts/nodejs-logging');
+import { AxiosError, AxiosInstance } from 'axios';
+import { AddressInfo } from '../types/AddressInfo';
+
+export class RptsApi {
+
+  private readonly baseURL = '/v1/search';
+  private readonly logger = Logger.getLogger('app');
+
+  constructor(
+    private readonly axios: AxiosInstance,
+  ) {}
+
+  public getCourts(postcode: string): Promise<AddressInfo> {
+    return this.axios
+      .get(`${this.baseURL}/address/` + postcode)
+      .then((results: any) => results.data)
+      .catch(err => {
+        this.logError(err);
+        return Promise.reject(err);
+      });
+  }
+
+  private logError(err: AxiosError) {
+    this.logger.error(err.message);
+
+    if (err.response) {
+      this.logger.info(err.response.data);
+      this.logger.info(err.response.headers);
+    }
+  }
+}
--- src/main/controllers/home.ts
+++ src/main/controllers/home.ts
@@ -1,27 +1,63 @@
 import { Application } from 'express';
+import { RptsApi } from '../api/rptsApi';
+import config from 'config';
+import axios from 'axios';
+import { AddressInfo } from '../types/AddressInfo';
 
 export default function (app: Application): void {
+
   app.get('/', (req, res) => {
     res.render('home');
   });
 
   app.get('/search', (req, res) => {
 
-    console.log(req.query);
-
     const postcode = req.query.postcode ? req.query.postcode as string :  '';
+    const postcodeIsValid = postcodeIsValidFormat(postcode);
 
-    res.render('search/content',  getOptions('', postcode,
-      postcode == '' && !req.query.initial, false));
+    if (postcode == '' || !postcodeIsValid) {
+      return res.render('search/content',  getOptions('', postcode,
+        postcode == '' && !req.query.initial,
+        !postcodeIsValid && !req.query.initial, {}));
+    }
+
+    new RptsApi(axios.create({
+      baseURL: config.get('services.api.url')
+    })).getCourts(postcode)
+      .then((value: AddressInfo) => {
+
+        console.log(value.addresses[0].DPA.ADDRESS);
+
+        return res.render('search/content',  getOptions('', postcode,
+          postcode == '' && !req.query.initial,
+          false, value));
+      })
+      .catch((err) => {
+        return res.render('search/content',  getOptions(err.request.res.statusCode == 500
+          ? 'Internal error: please contact your system administrator':
+          'No details found for provided postcode', postcode,
+          postcode == '' && !req.query.initial, false, {}));
+      });
   });
 
   function getOptions(errorMsg: string,  postcodeEntered: string,
-                      emptyValueFound: boolean, postCodeInvalidFormat: boolean) {
+                      emptyValueFound: boolean, postCodeInvalidFormat: boolean,
+                      addressInfo: any) {
     return {
       'errorMsg': errorMsg,
       'postcodeEntered': postcodeEntered,
       'emptyValueFound': emptyValueFound,
-      'postcodeInvalidFormat': postCodeInvalidFormat
+      'postcodeInvalidFormat': postCodeInvalidFormat,
+      'addressInfo': addressInfo
     };
+  }
+
+  function postcodeIsValidFormat(postcode: string): boolean {
+    const postcodeRegex = new RegExp('([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|' +
+      '(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))' +
+      '\\s?[0-9][A-Za-z]{2})', 'g');
+
+    const match = postcode.match(postcodeRegex);
+    return match?.length === 1 && match[0] === postcode;
   }
 }
