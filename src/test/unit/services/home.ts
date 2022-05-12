453c8fb054a32c3bed98bb4509657dd696c2b2e1
amstevenson <addstevenson@hotmail.com>
2022-05-12 17:24:02 +0100

    added unit test, refactored existing code structure

--- src/test/unit/services/home.ts
+++ src/test/unit/services/home.ts
@@ -1,8 +1,8 @@
 import { RptsApi } from '../../../main/api/rptsApi';
 import { HomeService } from '../../../main/services/HomeService';
 import { AddressInfo } from '../../../main/types/AddressInfo';
-import { mockRequest } from '../utils/mockRequest';
-import { mockResponse } from '../utils/mockResponse';
+import { mockRequest } from './utils/mockRequest';
+import { mockResponse } from './utils/mockResponse';
 
 const axios = require('axios');
 
