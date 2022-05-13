a248892e254a3fa30594e8815eb1320e006ae683
amstevenson <addstevenson@hotmail.com>
2022-05-13 13:52:29 +0100

    adding more tests

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -6,13 +6,13 @@ const testUrl = process.env.TEST_URL || 'http://localhost:3120';
 describe('Smoke Test', () => {
   describe('Home page loads', () => {
     test('with correct content', async () => {
-        const response: AxiosResponse = await axios.get(testUrl, {
-          headers: {
-            'Accept-Encoding': 'gzip',
-          },
-        });
-        expect(response.status).eq(200);
-        expect(response.data).includes('GOV.UK');
+      const response: AxiosResponse = await axios.get(testUrl, {
+        headers: {
+          'Accept-Encoding': 'gzip',
+        },
+      });
+      expect(response.status).eq(200);
+      expect(response.data).includes('GOV.UK');
     });
   });
 });
