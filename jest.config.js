d154bc3fc3a6e9d426ee4b70970f5c232868705f
amstevenson <addstevenson@hotmail.com>
2023-02-06 16:36:40 +0000

    reverting

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src/test/unit'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
-  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ["src/test/unit/utils"]
 };
--- jest.smoke.config.js
+++ jest.smoke.config.js
@@ -7,15 +7,12 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
-  reporters: [
-    'default',
-    [
-      './node_modules/jest-html-reporter',
-      {
-        pageTitle: 'Smoke Test Report',
-        outputPath: './smoke-output/reports/test-report.html',
-        includeFailureMsg: true,
-      },
-    ],
-  ],
+  "reporters": [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      "pageTitle": "Smoke Test Report",
+      "outputPath": "./smoke-output/reports/test-report.html",
+      "includeFailureMsg": true
+    }]
+  ]
 };
