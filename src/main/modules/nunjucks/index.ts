652385a650132797bb1863d554d6f5a9cde01e17
josh-uk <joshua.blackmoor@justice.gov.uk>
2025-05-22 21:35:52 +0100

    enabled rebrand

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -11,11 +11,12 @@ export class Nunjucks {
   enableFor(app: express.Express): void {
     app.set('view engine', 'njk');
     const govUkFrontendPath = path.join(__dirname, '..', '..', '..', '..', '.yarn/cache', 'govuk-frontend');
-    nunjucks.configure([path.join(__dirname, '..', '..', 'views'), govUkFrontendPath], {
+    const nunjucksEnv = nunjucks.configure([path.join(__dirname, '..', '..', 'views'), govUkFrontendPath], {
       autoescape: true,
       watch: this.developmentMode,
       express: app,
     });
+    nunjucksEnv.addGlobal('govukRebrand', true);
 
     app.use((req, res, next) => {
       res.locals.pagePath = req.path;
